950b3b1c7ba75f419a89b6ccef00db004f384bc4
MustGet100 <akshaypn@hotmail.com>
2017-06-14 14:04:33 -0500

    Create vigenere.c

---
+++ vigenere.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+int main(int argc, string argv[])
+{
+    string c=(argv[1]);
+    for (int i = 0; i < strlen(c); i++)
+    {   
+        if (!isalpha(c[i]))
+        {
+            printf("Must be alphabet. Try again.\n");
+            return 1;
+        }
+    } 
+    printf("plaintext: ");
+    string s = get_string();
+    printf("ciphertext: ");
+    if (argc==2) {
+    for (int q = 0, n = strlen(s); q < n; q++)
+       {   
+        for (int l = 0, j = strlen(c); l < j; l++){
+            if (isupper(s[q]))
+            {
+               printf("%c",((s[l]+toupper(c[q])-65)%26+65));
+            }
+            else if (islower(s[q]))
+            {
+               printf("%c",((s[l]+tolower(c[q])-97)%26+97));
+            }
+            else {
+                printf("%c",s[l]);
+            }
+        }
+        printf("\n");
+        return(0);           
+    if(argc!=2) {
+    printf("Usage:./viganerek");
+    return(1);
+}
+}
+}
+}
